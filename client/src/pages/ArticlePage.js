f4eb6495637be811c2d16993a77dc55846e52103
curgery <androidgeek54@gmail.com>
2020-01-07 21:44:24 -0500

    provided a git heroku clone to app

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -7,39 +7,42 @@ import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
-    const name = match.params.name;
-    const article = articleContent.find(article => article.name === name);
+  const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
 
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await fetch(`/api/articles/${name}`);
-            const body = await result.json();
-            console.log(body);
-            setArticleInfo(body);
-        }
-        fetchData();
-    }, [name]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+  if (!article) return <NotFoundPage />;
 
-    if (!article) return <NotFoundPage />
+  const otherArticles = articleContent.filter(article => article.name !== name);
 
-    const otherArticles = articleContent.filter(article => article.name !== name);
-
-    return (
-        <>
-        <h1>{article.title}</h1>
-        <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
-        <p>This post has been upvoted {articleInfo.upvotes} times</p>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <CommentsList comments={articleInfo.comments} />
-        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
-        <h3>Other Articles:</h3>
-        <ArticlesList articles={otherArticles} />
-        </>
-    );
-}
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <UpvotesSection
+        articleName={name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
+    </>
+  );
+};
 
 export default ArticlePage;
