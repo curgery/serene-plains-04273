02c6520f29fd8c7b0be2bf9dd8d69303da6cc457
curgery <androidgeek54@gmail.com>
2020-01-07 22:44:22 -0500

    simplay changed location of PORT definition

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 
 const uri =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/heroku_hq0frgvm';
-const port = process.env.PORT || 5000;
 
 const app = express();
 // app.use(express.static(path.join(__dirname, '/client/build')));
@@ -95,6 +94,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
   });
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
