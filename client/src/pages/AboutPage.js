8a3380388ffca7c856421a6c644310aa86042798
curgery <androidgeek54@gmail.com>
2020-01-08 10:04:26 -0500

    corrected a date

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,77 +1,101 @@
 import React from 'react';
 
 const AboutPage = () => (
-    <>
-   
+  <>
     <h1>Curriculum Vitae </h1>
-
-<p>
-Address:<br/>
-461 Riverdale Avenue<br/>
-Yonkers, NY 10705<br/>
-Contact: (914) 536-3128<br/>
-<a href="mailto:rglover898@optonline.net">Send email to:  rglover898@optonline.net</a><br/><br/>
-Development Projects:<br/>
-<a href="https://github.com/curgery/">https://github.com/curgery</a><br/><br/>
-Blog/CV:<br/>
-<a href="https://calm-falls-63576.herokuapp.com/">https://rlglover.com/</a><br/>
-
-</p>
-<ul>								
-<h4>
-User interface developer engaged in front end development, mobile apps, website applications, accessibility + usability, workflows for digital publishing, communications, brand expansion and business strategies.
-</h4>
- </ul>	
-
-<h3>Degrees</h3>
-<li>- Bronx Community College, Montefiore Enhanced HIT Analyst Certificate in Clinical Looking Glass (CLG) Big Data,          June 2014, Bronx, NY  10453.</li>
-<li>- Skidmore College, Master’s Certificate, Computer Interfaces , June 2005, White Plains, NY 10601</li>
-<li>- Manhattan College, Bachelor of Science, Business Administration ,  June 1982, Riverdale, NY 10471
-</li>
-
-<h3>Professional Employment</h3> 
-<span>Contract Officer, USPS/Operations Programs Support, White Plains, NY 10610-9997, November 1985 – October 2014</span><br/><br/>
-
-<li>-Responsible for coordinating and processing the deployment of mail operations by identifying patterns of classes, volume and production activity, through zip code directories, required to dispatch and coordinate delivery for this central processing distribution center. Designed and implemented UX frontend interfacing to achieve and support these goals. </li><br/>
-
-<li>Freelance Web/Mobile Developer, 2004-present
-Providing small-scale web design, development, and consulting services for corporations and small businesses, publishers, academic digital humanities projects, and other digital publishing ventures.</li><br/>
-
-
-<h4>Notable Contractor Projects and Clients:</h4>
-<ul></ul>
-<li>AT&T FOUNDATION: Philanthropy Administation, Gift for Windows</li>
-<li>WINSTON AGENCY: Employee Administration Dashboard</li>
-<li>SIEMPRE ENERGY: Frontend Remote Subsidiary Administration</li>
-<li>HACHETTE FILLIPACHI: Accounting Frontend Management </li> 
-
-<h4>
-Technology Applications and Expertise
-</h4>
-
-<h5>
-Specialized Skills
-</h5>
-<ul></ul>
-User Interface Design, User Testing, Front End Web Development,& Digital Publishing Workflows
-
-<h5>Publishing Platforms</h5>
-Jekyll, GatsbyJS, ReactJS, React Native with Expo, Github Pages
-
-<h5>Applications</h5>
-Visual Studio Code, Microsoft Office Google Drive, Windows Subsytem for Linux
-
-<h5>OS/Environments</h5>
-Windows, Linux, Ubuntu, & Mac
-
-<h5>Markup/Programming</h5>
-JavaScript, HTML5,  CSS, CSS3, Bootstrap, Awesome Fonts, Ruby/Rails,  XML, MySQL, Postgres, MongoDB, & GraphQL
-
-<h5>Version Control</h5>
-Git, GitHub, CVS
-
-
-</>
+    <p>
+      Address:
+      <br />
+      461 Riverdale Avenue
+      <br />
+      Yonkers, NY 10705
+      <br />
+      Contact: (914) 536-3128
+      <br />
+      <a href='mailto:rglover898@optonline.net'>
+        Send email to: rglover898@optonline.net
+      </a>
+      <br />
+      <br />
+      Development Projects:
+      <br />
+      <a href='https://github.com/curgery/'>https://github.com/curgery</a>
+      <br />
+      <br />
+      Blog/CV:
+      <br />
+      <a href='https://calm-falls-63576.herokuapp.com/'>
+        https://rlglover.com/
+      </a>
+      <br />
+    </p>
+    <ul>
+      <h4>
+        User interface developer engaged in front end development, mobile apps,
+        website applications, accessibility + usability, workflows for digital
+        publishing, communications, brand expansion and business strategies.
+      </h4>
+    </ul>
+    <h3>Degrees</h3>
+    <li>
+      - Bronx Community College, Montefiore Enhanced HIT Analyst Certificate in
+      Clinical Looking Glass (CLG) Big Data, June 2014, Bronx, NY 10453.
+    </li>
+    <li>
+      - Skidmore College, Master’s Certificate, Computer Interfaces , June 2005,
+      White Plains, NY 10601
+    </li>
+    <li>
+      - Manhattan College, Bachelor of Science, Business Administration , June
+      1982, Riverdale, NY 10471
+    </li>
+    <h3>Professional Employment</h3>
+    <span>
+      Contract Officer, USPS/Operations Programs Support, White Plains, NY
+      10610-9997, November 1985 – October 2015
+    </span>
+    <br />
+    <br />
+    <li>
+      -Responsible for coordinating and processing the deployment of mail
+      operations by identifying patterns of classes, volume and production
+      activity, through zip code directories, required to dispatch and
+      coordinate delivery for this central processing distribution center.
+      Designed and implemented UX frontend interfacing to achieve and support
+      these goals.{' '}
+    </li>
+    <br />
+    <li>
+      Freelance Web/Mobile Developer, 2004-present Providing small-scale web
+      design, development, and consulting services for corporations and small
+      businesses, publishers, academic digital humanities projects, and other
+      digital publishing ventures.
+    </li>
+    <br />
+    <h4>Notable Contractor Projects and Clients:</h4>
+    <ul></ul>
+    <li>AT&T FOUNDATION: Philanthropy Administation, Gift for Windows</li>
+    <li>WINSTON AGENCY: Employee Administration Dashboard</li>
+    <li>SIEMPRE ENERGY: Frontend Remote Subsidiary Administration</li>
+    <li>HACHETTE FILLIPACHI: Accounting Frontend Management </li>
+    <h4>Technology Applications and Expertise</h4>
+    <h5>Specialized Skills</h5>
+    <ul></ul>
+    User Interface Design, User Testing, Front End Web Development,& Digital
+    Publishing Workflows
+    <h5>Publishing Platforms</h5>
+    Jekyll, GatsbyJS, ReactJS, React Native with Expo, Github Pages
+    <h5>Applications</h5>
+    Visual Studio Code, Microsoft Office Google Drive, Windows Subsytem for
+    Linux
+    <h5>OS/Environments</h5>
+    Windows, Linux, Ubuntu, & Mac
+    <h5>Markup/Programming</h5>
+    JavaScript, HTML5, CSS, CSS3, Bootstrap, Awesome Fonts, Ruby/Rails, XML,
+    MySQL, Postgres, MongoDB, & GraphQL
+    <h5>Version Control</h5>
+    Git, GitHub, CVS
+  </>
 );
 
 export default AboutPage;
